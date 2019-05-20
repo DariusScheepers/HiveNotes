a6626daf3e2c74a31b3fa19ee392ca6421c849e5
Aimzj <aimeejhandley@gmail.com>
2019-05-20 21:17:22 +0200

    Basic nav-bar set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainersModule } from './Containers/containers.module';
+import { SideNavComponent } from './Containers/common/side-nav/side-nav.component';
+
+import { MatSidenavModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ContainersModule
+    ContainersModule,
+
+    MatSidenavModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
