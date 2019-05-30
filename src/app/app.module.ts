bde17cc77556b7684631d740018cd789d43b6ea8
Marno <njmgrobler@gmail.com>
2019-05-30 15:20:27 +0200

    Made buttons pretty and layout and a bunch of stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainersModule } from './Containers/containers.module';
 import { CommonComponentsModule } from './Containers/common/common-components.module';
-import { HttpClientModule }    from '@angular/common/http';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
